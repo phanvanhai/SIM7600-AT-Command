325dc65c58fc2ab74ac24599bf4fa0d2475628f8
phanvanhai <hai.phanv.hust@gmail.com>
2020-12-29 14:04:30 +0700

    update GPS

--- python/test_sim.py
+++ python/test_sim.py
@@ -1,14 +1,6 @@
 import sys
 import sim
 import time
-import argparse
-
-Debug = False
-parser = argparse.ArgumentParser()
-parser.add_argument("-debug", action="store_true")
-args = parser.parse_args()
-if args.debug:
-    Debug = True
 
 URL = 'http://httpbin.org/post'
 ContentType = 'application/json'
@@ -23,8 +15,8 @@ POWER_KEY = 6
 
 # ---------------------- MAIN ---------------------
 # init SIM
-# sim.power_on(POWER_KEY)
-ok = sim.at_init(SIM_SERIAL_PORT, SIM_SERIAL_BAUD, Debug)
+sim.power_on(POWER_KEY)
+ok = sim.at_init(SIM_SERIAL_PORT, SIM_SERIAL_BAUD, False)
 if not ok:
     print('SIM AT init error')
     sys.exit(1)
@@ -39,23 +31,23 @@ try:
         # Get Time
         time_sim = sim.time_get()
         if time_sim != '':
-            if Debug: print('Time:' + time_sim)
+            print('Time:' + time_sim)
         
         # time.sleep(2)
-        # Get GPS            
+        # Get GPS    
+        count = count + 1        
         gps, ok = sim.gps_get_data()
-        if ok:
-            if Debug: print('GPS:' + gps)
+        if ok:            
+            print('Time:' + str(count) + ' GPS:' + gps)
         else:
-            if Debug: print('GPS not ready')        
+            print('Time:' + str(count) + ' GPS not ready')
         # time.sleep(2)
-        # POST HTTP
-        if Debug:
-            print('Send data to Server:' + URL)
-            print(body)
+        # POST HTTP        
+        print('Send data to Server:' + URL)
+        print(body)
         ok = sim.http_post(URL, ContentType, body, '',HTTP_CONNECT_TIMEOUT, HTTP_RESPONSE_TIMEOUT)
         if not ok:
-            if Debug: print('Error send data to Server')        
+            print('Error send data to Server')        
     
 except KeyboardInterrupt:
     main_is_run = False
